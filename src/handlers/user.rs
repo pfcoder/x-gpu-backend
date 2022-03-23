b9d76fcacf2d8568a001bac2c5f0491512de68a6
pfcoder <pfcoder97@gmail.com>
2022-03-23 14:44:53 +0800

    fix sso

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -22,6 +22,10 @@ struct SsoTokenResponse {
     scope: String,
 }
 
+/*
+https://sso.codegene.xyz/api/login/oauth/access_token?grant_type=authorization_code&client_id=66e22804c3d5772b80d6&client_secret=40f6e13382ec381103ce5c7253fdbf4a869201c1&code=e5dd6086027216e45c93' \
+*/
+
 #[handler]
 pub async fn sso_cb(
     Query(SsoLoginCallbackParam { code, state }): Query<SsoLoginCallbackParam>,
@@ -30,15 +34,10 @@ pub async fn sso_cb(
     let client = reqwest::Client::new();
     let setting = get_configuration().unwrap();
 
-    let mut map = HashMap::new();
-    map.insert("grant_type", "authorization_code");
-    map.insert("client_id", &setting.sso.client_id);
-    map.insert("client_secret", &setting.sso.client_secret);
-    map.insert("code", &code);
-
     let token_res = client
-        .post("https://sso.codegene.xyz/api/login/oauth/access_token")
-        .json(&map)
+        .post(format!(
+            "https://sso.codegene.xyz/api/login/oauth/access_token?grant_type=authorization_code&client_id={}&client_secret={}&code={}", setting.sso.client_id, setting.sso.client_secret, code
+        ))
         .send()
         .await
         .map_err(BadRequest)?
